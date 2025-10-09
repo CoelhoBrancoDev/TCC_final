cc549bd0854259094e406f76784ffd5aad3da120
adriano <adrianomacielhsr@gmail.com>
2025-10-09 16:41:25 -0300

    criação da tela dashboard analitico

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { CatalogoComponent } from './catalogo/catalogo.component';
 import { Home } from './home/home';
 import { QuemSomosComponent } from './quem-somos/quem-somos.component';
 import { ContatoComponent } from './contato/contato.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 
 
@@ -37,4 +38,12 @@ export const routes: Routes = [
   pathMatch: 'full',
   data: { title: 'Contato - Super Atleta' }
 },
+
+{ 
+  path: 'dashboard', 
+  component: DashboardComponent,
+  pathMatch: 'full',
+  data: { title: 'Dashboard Analítico - Super Atleta' }
+},
+
 ];
